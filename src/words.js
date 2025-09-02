944374f1475ca78791a4962153a7ccecade6e94b
Ajay Gangisetti <ajay.g415@gmail.com>
2025-09-02 22:41:12 +0530

    Create words.js

---
+++ src/words.js
@@ -0,0 +1,9 @@
+// src/words.js
+// Returns a list of 3-letter English words for the game
+
+const words = [
+  'cat', 'dog', 'sun', 'car', 'box', 'hat', 'bat', 'fan', 'pen', 'cup',
+  'map', 'run', 'top', 'red', 'bed', 'man', 'jam', 'lip', 'web', 'fix'
+];
+
+export default words;
